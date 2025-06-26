aedb851b0015a0c72cffb41510ed3bf8fc73c848
Nischal <nyyscal541556@gmail.com>
2025-06-26 18:20:41 +0545

    Minor Error Fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cocktails from "./components/Cocktails"
-import About from "./components/ABout"
+import About from "./components/About"
 import Art from "./components/Art"
 import Menu from "./components/Menu"
 import Footer from "./components/Footer"
@@ -12,7 +12,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-   <main>
+   <main className="overflow-auto scrollbar-hide">
     <Navbar/>
     <Hero/>
     <Cocktails/>
